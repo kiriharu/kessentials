ad1bfe4629b3b1f5c768d5c2281965d4923138d1
kiriharu <kiriharu@yandex.ru>
2020-10-23 00:36:43 +0300

    added ServerPlayerExtensions.cs for easy send messages

---
+++ KVSL/src/Utils/ServerPlayerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace Kvsl.Utils
+{
+    public static class ServerPlayerExtensions
+    {
+
+        public static void SendErr(this IServerPlayer serverPlayer, string message, string data = null)
+        {
+            serverPlayer.SendMessage(GlobalConstants.GeneralChatGroup, message, EnumChatType.CommandError, data);
+        }
+
+        public static void SendOk(this IServerPlayer serverPlayer, string message, string data = null)
+        {
+            serverPlayer.SendMessage(GlobalConstants.GeneralChatGroup, message, EnumChatType.CommandSuccess, data);
+        }
+
+        public static void SendMsg(this IServerPlayer serverPlayer, string message, string data = null)
+        {
+            serverPlayer.SendMessage(GlobalConstants.GeneralChatGroup, message, EnumChatType.OwnMessage, data);
+        }
+        
+    }
+}
